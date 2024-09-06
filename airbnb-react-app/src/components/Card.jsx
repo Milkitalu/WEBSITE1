21ae656e2fe1da2e27a4cc327492af8425910e3f
Talaku Getahun <getahuntalaku@gmail.com>
2024-09-06 21:37:38 +0300

    after changing my code to recieve dynamic props

--- airbnb-react-app/src/components/Card.jsx
+++ airbnb-react-app/src/components/Card.jsx
@@ -1,25 +1,26 @@
 
 
-function Card (){
+function Card (props){
+    console.log(props)
     return(
         <div className="card">
         <img 
-            src="../images/katie-zaferes.png" 
+            src={`../images/${props.img}`}
             className="card--image" 
             alt="Main card image."
         />
-        <div className="card--stats">
+        <div className="card--stats"> 
             <img 
                 src="../images/star.png" 
                 className="card--star" 
                 alt="Star icon."
-            />
-            <span>5.0</span>
-            <span className="gray">(6) • </span>
-            <span className="gray">USA</span>
+            /> 
+            <span>{props.rating}</span>
+            <span className="gray">({props.reviewCount}) • </span>
+            <span className="gray">{props.country}</span>
         </div>
         <h2>Life Lessons with Katie Zaferes</h2>
-        <p><span className="bold">From $136</span> / person</p>
+        <p><span className="bold">From ${props.price}</span> / person</p>
     </div>
     );
 }
